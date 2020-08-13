a68146623ed14a7297e6c95fc27c6299229e1260
Jaya <jayamisra02@gmail.com>
2020-08-13 16:53:36 -0400

    dockerfile

--- calculator_calculator/src/test/java/com/networthcalculator/calculator/NetWorthRepositoryTest.java
+++ calculator_calculator/src/test/java/com/networthcalculator/calculator/NetWorthRepositoryTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertFalse;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
